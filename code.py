f724ef604c2e2a95036c319ca1120958f10ec6e0
d0773d <dottedquad@gmail.com>
2023-05-20 21:19:59 -0700

    Update code.py

--- code.py
+++ code.py
@@ -6,14 +6,20 @@ from ezo_sensors import Ezo
 # from adafruit_ht16k33.segments import Seg7x4
 
 # instantiate the object
-humidity = Ezo(0x6F, False)
+humidity = Ezo(0x6F, "hum", False)
+res = Ezo(0x66, "tmp", False)
+ph = Ezo(0x63, "ph", False)
 
 while True:
-    time.sleep(5)
-    print("test2")
+    time.sleep(1)
 
     # call our instance methods
     # humidity.cmd_enable_all_paramaters()
     # humidity.cmd_set_name()
+    #ph.cmd_set_phplock()
 
     humidity.cmd_r()
+    time.sleep(1)
+    res.cmd_r()
+    time.sleep(1)
+    ph.cmd_r()
