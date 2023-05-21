dfdf4ab52fe12a48054fcfdcc73774e1963cb54d
d0773d <dottedquad@gmail.com>
2023-05-20 18:46:33 -0700

    Update ezo_sensors.py

--- ezo_sensors.py
+++ ezo_sensors.py
@@ -2,17 +2,23 @@ import board
 import busio
 import time
 
+from adafruit_bus_device.i2c_device import I2CDevice
+i2c = busio.I2C(board.SCL, board.SDA)
+
 class Ezo:
     # instance attribute
-    def __init__(self, i2c_addr, seven_seg_output=False, touch_scr_output=False):
-        from adafruit_bus_device.i2c_device import I2CDevice
+    def __init__(self, i2c_addr, sensor_type, seven_seg_output=False, touch_scr_output=False):
+        #print(sensor_type)
+        # Create library object on our I2C port
+        self.device = I2CDevice(i2c, i2c_addr)
 
         self.ezo_sensor_settings = {
             "device_address": i2c_addr,
+            "sensor_type": sensor_type,
             "short_wait": 3,
             "long_wait": 5,
             "largest_string": 24,
-            "smallest_string": None,
+            "smallest_string": 4,
             "name": None,
             "units": None
         }
@@ -24,14 +30,14 @@ class Ezo:
             "air_temperature": None
         }
 
-        self.i2c = busio.I2C(board.SCL, board.SDA)
+        #self.i2c = busio.I2C(board.SCL, board.SDA)
 
         # Create library object on our I2C port
-        self.device = I2CDevice(self.i2c, i2c_addr)
+        #self.device = I2CDevice(self.i2c, i2c_addr)
 
     def cmd_set_name(self):
         cmd = "Name,hum1"
-        
+
         # Encode cmd variable to a bytearray
         cmd = cmd.encode()
 
@@ -44,14 +50,14 @@ class Ezo:
     def cmd_enable_all_paramaters(self):
         # Send O,Dew,1 command
         # Enables all the parameters
-        
+
         cmd = "O,Dew,1"
-        
+
         # Encode cmd variable to a bytearray
         cmd = cmd.encode()
 
         time.sleep(3)
-        
+
         # Set the result buffer with the Sensor data result
         with self.device:
             self.device.write(cmd)
@@ -78,7 +84,8 @@ class Ezo:
             raw_result = bytearray(self.ezo_sensor_settings["largest_string"])
             self.device.readinto(raw_result)
 
-            if raw_result[0] == 1:             # if the response isn't an error
+            # if the response isn't an error
+            if raw_result[0] == 1:
                 # Convert bytearray to string
                 str_result = ''.join([chr(b) for b in raw_result])
 
@@ -87,17 +94,18 @@ class Ezo:
 
                 #remove \x00 from the end of the string
                 str_result = str_result.replace('\x00','')
-                
-                # Populate humidity sensor values
-                self.ezo_sensor_values["relative_humidity"] = str_result.split(',')[0]
-                self.ezo_sensor_values["air_temperature"] = str_result.split(',')[1]
-                self.ezo_sensor_values["dew_point"] = str_result.split(',')[3]
 
-                print(str_result)
-                print(self.ezo_sensor_values["relative_humidity"])
-                print(self.ezo_sensor_values["air_temperature"])
-                print(self.ezo_sensor_values["dew_point"])
-            else:
-                # Output the response value if it is anything other than a 1
-                print("ERROR")
-                #print(str(response[0]))
+                if self.ezo_sensor_settings["sensor_type"] == "hum":
+                    # Populate humidity sensor values
+                    self.ezo_sensor_values["relative_humidity"] = str_result.split(',')[0]
+                    self.ezo_sensor_values["air_temperature"] = str_result.split(',')[1]
+                    self.ezo_sensor_values["dew_point"] = str_result.split(',')[3]
+
+                    print(str_result)
+                    print(self.ezo_sensor_values["relative_humidity"])
+                    print(self.ezo_sensor_values["air_temperature"])
+                    print(self.ezo_sensor_values["dew_point"])
+                
+                if self.ezo_sensor_settings["sensor_type"] == "tmp":
+                    print("Res Temp")
+                    print(str_result)
