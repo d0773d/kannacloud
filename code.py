ddb128c9efab7f4e791c288819a22d994ca0bc43
d0773d <dottedquad@gmail.com>
2023-11-16 14:03:26 -0800

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ import asyncio
 import board
 import busio
 import time
+import os
+import sys
 
 from multiprocessing import Process
 
@@ -22,14 +24,25 @@ def poll_sensors():
         ezo.poll_sensors(ezo.get_sensor_types_addresses())
 
 if init.init_status() is False:
+    print("Grab Sensors and Sensor Settings")
     init.initialize_devices()
 else:
     print("Initialize: ", True)
 
-    # Create a new process with a specified function to execute.
-    uart_monitor = Process(target=monitor_nextion)
-    poll_sensors = Process(target=poll_sensors)
+# Create a new process with a specified function to execute.
+uart_monitor = Process(target=monitor_nextion)
+poll_sensors = Process(target=poll_sensors)
 
-    # Run the new process
-    uart_monitor.start()
-    poll_sensors.start()
+# Run the new process
+#uart_monitor.start()
+poll_sensors.start()
+
+while True:
+    time.sleep(15)
+
+    if poll_sensors.is_alive():
+        print("Alive")
+    else:
+        #Restart the script if poll_sensors isn't alive.
+
+        os.execv(sys.executable, ['python3'] + sys.argv)
