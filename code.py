e0028b32f75f0a93a063bc30516447a975ef1891
d0773d <dottedquad@gmail.com>
2023-11-25 14:06:08 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-]import asyncio
+import asyncio
 import board
 import busio
 import time
@@ -21,28 +21,36 @@ def monitor_nextion():
         nextion.monitor_nextion()
 
 def poll_sensors(triggers_actions):
-    # Create a dictionary dynamically based on sensor type
-    sensor_dict = {}
+    # Load JSON data
+    parsed_data = triggers_actions
+
+    # Dynamically create dictionaries
+    sensor_types_dict = {}
+    action_dict = {}
 
     # Iterate through triggers
-    for trigger in triggers_actions['triggers']:
+    for trigger in parsed_data['triggers']:
         for sensor_type, triggers_list in trigger.items():
+            if sensor_type not in sensor_types_dict:
+                sensor_types_dict[sensor_type] = []
+            
             for trigger_info in triggers_list:
                 action_name = trigger_info['action_name']
-                value = trigger_info['value']
+                sensor_types_dict[sensor_type].append(action_name)
                 
-                # Create dictionary dynamically based on sensor type
-                if sensor_type not in sensor_dict:
-                    sensor_dict[sensor_type] = {}
-                
-                sensor_dict[sensor_type][value] = action_name
+                # Create dictionary for action
+                action_dict[action_name] = parsed_data['actions'][0][action_name]
 
-    # Print the created dictionary
-    print("Sensor Dictionary:", sensor_dict)
-    while True:
-        print()
-        #print(triggers_actions["triggers"]["pH"])
-        ezo.poll_sensors(ezo.get_sensor_types_addresses())
+    '''# Print the created dictionaries
+    print("Sensor Types Dictionary:", sensor_types_dict)
+    print("\nAction Dictionary:")
+    for action_name, actions in action_dict.items():
+        print(f"{action_name}: {actions}")'''
+
+
+    print(sensor_types_dict)
+    print()
+    print(action_dict)
 
 triggers_actions = init.get_triggers_and_actions()
 
